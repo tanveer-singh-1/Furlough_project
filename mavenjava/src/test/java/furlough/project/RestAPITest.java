7ceb04afef32e8ab3dba506a8d9c54e5dff34af7
TanveerSingh <TanveerSingh@DESKTOP-M2RVEQJ>
2019-12-31 16:50:39 +0530

    commit

---
+++ mavenjava/src/test/java/furlough/project/RestAPITest.java
@@ -0,0 +1,14 @@
+package furlough.project;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void weatherAPI()
+	{
+		
+		System.out.print("WEATHER API");
+	}
+	
+}
