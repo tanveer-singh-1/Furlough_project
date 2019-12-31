a79147b7b0e9191734e1a3689c775448c374c230
TanveerSingh <TanveerSingh@DESKTOP-M2RVEQJ>
2019-12-31 16:59:49 +0530

    commit

--- mavenjava/src/test/java/furlough/project/RestAPITest.java
+++ mavenjava/src/test/java/furlough/project/RestAPITest.java
@@ -1,13 +1,24 @@
 package furlough.project;
 
+import static io.restassured.RestAssured.given;
+
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class RestAPITest {
 
 	@Test
 	public void weatherAPI()
 	{
-		
+		RestAssured.baseURI="http://restapi.demoqa.com";
+
+		given().
+		when().
+			get("/utilities/weatherfull/city/jammu").
+			
+			then().assertThat().statusCode(200).and().contentType(ContentType.JSON);
 		System.out.print("WEATHER API");
 	}
 	
