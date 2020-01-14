c117e7812c2f5f276821b26e71c0833d41e7a291
TanveerSingh <TanveerSingh@DESKTOP-M2RVEQJ.in.ibm.com>
2020-01-14 12:06:07 +0530

    commit

--- mavenjava/src/test/java/furlough/project/RestAPITest.java
+++ mavenjava/src/test/java/furlough/project/RestAPITest.java
@@ -18,7 +18,7 @@ public class RestAPITest {
 		when().
 			get("/utilities/weatherfull/city/jammu").
 			
-			then().assertThat().statusCode(200).and().contentType(ContentType.JSON);
+			then().assertThat().statusCode(200);
 		System.out.print("WEATHER API");
 	}
 	
